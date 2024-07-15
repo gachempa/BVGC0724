b4ef31c81e49afd81baa3dd0b0674da47c55b177
gachempa <jpsg514@gmail.com>
2024-07-15 15:13:54 -0700

    iss API and text file

--- API-1/API_ISS.py
+++ API-1/API_ISS.py
@@ -3,5 +3,20 @@ import turtle
 import urllib.request
 import time
 import webbrowser
-import geocoder
+# import geocoder
+
+url = "http://api.open-notify.org/astros.json"
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+# print(result)
+
+file = open("iss.txt","w")
+file.write(
+    "There are currently " + str(result["number"]) +
+    " astronauts on the ISS: \n\n"
+)
+
+people = result["people"]
+for p in people:
+    file.write(p['name'] + " - on board" + "\n")
 
